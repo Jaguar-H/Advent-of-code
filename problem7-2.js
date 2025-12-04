a8a28e2cf610dc1921aca979cde981003be79891
Himanshu Jaguri <himanshujaguri@Himanshus-MacBook-Pro.local>
2025-12-04 23:59:21 +0530

    question 8

--- problem7-2.js
+++ problem7-2.js
@@ -1,39 +1,15 @@
 import { permutation } from "./problem7.js";
 
 const program = [
-  3,
-  26,
-  1001,
-  26,
-  -4,
-  26,
-  3,
-  27,
-  1002,
-  27,
-  2,
-  27,
-  1,
-  27,
-  26,
-  27,
-  4,
-  27,
-  1001,
-  28,
-  -1,
-  28,
-  1005,
-  28,
-  6,
-  99,
-  0,
-  0,
-  5,
+3,52,1001,52,-5,52,3,53,1,52,56,54,1007,54,5,55,1005,55,26,1001,54,
+-5,54,1105,1,12,1,53,54,53,1008,54,0,55,1001,55,1,55,2,53,55,53,4,
+53,1001,56,-1,56,1005,56,6,99,0,0,0,0,10
 ];
 
-export function run(program, inputValue, code, ip, flag,inputs) {
-  const mem = [...program];
+1336480
+//1320024
+export function run(program, inputValue, code, ip, flag, inputs) {
+  const mem = program;
 
   const get = (mode, idx) => mode === 0 ? mem[mem[idx]] : mem[idx];
 
@@ -62,7 +38,7 @@ export function run(program, inputValue, code, ip, flag,inputs) {
       const [mode1] = modes();
       const val = get(mode1, ip + 1);
       ip += 2;
-      return [val, ip, false,inputs];
+      return [val, ip, false, inputs];
       // console.log(val);
     },
     5: () => {
@@ -99,47 +75,47 @@ export function run(program, inputValue, code, ip, flag,inputs) {
     if (out !== undefined) return out;
   }
 
-  return [inputValue, ip, true,inputs];
+  return [inputValue, ip, true, inputs];
 }
 
 const allPossiblepermutation = permutation([9, 7, 8, 5, 6]);
 let maxThrust = -Infinity;
 
-// for (const i of allPossiblepermutation) {
-const amp1 = [...program];
-const amp2 = [...program];
-const amp3 = [...program];
-const amp4 = [...program];
-const amp5 = [...program];
+for (const i of allPossiblepermutation) {
+  const amp1 = [...program];
+  const amp2 = [...program];
+  const amp3 = [...program];
+  const amp4 = [...program];
+  const amp5 = [...program];
 
-let i1 = 0;
-let i2 = 0;
-let i3 = 0;
-let i4 = 0;
-let i5 = 0;
+  let i1 = 0;
+  let i2 = 0;
+  let i3 = 0;
+  let i4 = 0;
+  let i5 = 0;
 
-let in1 = 0;
-let in2 = 0;
-let in3 = 0;
-let in4 = 0;
-let in5 = 0;
+  let in1 = 0;
+  let in2 = 0;
+  let in3 = 0;
+  let in4 = 0;
+  let in5 = 0;
 
-let f1 = false;
-let f2 = false;
-let f3 = false;
-let f4 = false;
-let f5 = false;
+  let f1 = false;
+  let f2 = false;
+  let f3 = false;
+  let f4 = false;
+  let f5 = false;
 
-const [a, b, c, d, e] = [9, 8, 7, 6, 5];
-let out = 0;
-while (!(f1 && f2 && f3 && f4 && f5)) {
-  [out, i1, f1,in1] = run(amp1, out, a, i1, f1, in1);
-  [out, i2, f2,in2] = run(amp2, out, b, i2, f2, in2);
-  [out, i3, f3,in3] = run(amp3, out, c, i3, f3, in3);
-  [out, i4, f4,in4] = run(amp4, out, d, i4, f4, in4);
-  [out, i5, f5,in5] = run(amp5, out, e, i5, f5, in5);
-  console.log(i1, i2, i3, i4, i5);
+  const [a, b, c, d, e] = i;
+  let out = 0;
+  while (!(f1 && f2 && f3 && f4 && f5)) {
+    [out, i1, f1, in1] = run(amp1, out, a, i1, f1, in1);
+    [out, i2, f2, in2] = run(amp2, out, b, i2, f2, in2);
+    [out, i3, f3, in3] = run(amp3, out, c, i3, f3, in3);
+    [out, i4, f4, in4] = run(amp4, out, d, i4, f4, in4);
+    [out, i5, f5, in5] = run(amp5, out, e, i5, f5, in5);
+    console.log(i1, i2, i3, i4, i5);
+  }
+  maxThrust = out > maxThrust ? out : maxThrust;
 }
-maxThrust = out > maxThrust ? out : maxThrust;
-// }
 console.log(maxThrust);
